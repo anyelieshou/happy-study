ab62e44893b54438cfece163987b4033b72ed2cd
ljs <1085170974@qq.com>
2020-05-28 19:06:47 +0800

    idea commit

--- src/main/java/com/happystudy/controller/StudentController.java
+++ src/main/java/com/happystudy/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.happystudy.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author LJS
- * @data 2020/5/28 19:01
+ * @data 2020/5/28 19:05
  */
+@Controller
 public class StudentController {
 }
